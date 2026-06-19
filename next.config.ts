b9ec82190a094ce3aa139f7179e574c8c975a28f
vrzsnchz23 <lctc.edsanchez@gmail.com>
2026-06-19 12:14:40 -0500

    Fix CSP: remove strict-dynamic that was breaking React hydration on client components

--- next.config.ts
+++ next.config.ts
@@ -8,7 +8,7 @@ const securityHeaders = [
     key: "Content-Security-Policy",
     value: [
       "default-src 'self'",
-      "script-src 'self' 'unsafe-inline' 'strict-dynamic' https://js.stripe.com",
+      "script-src 'self' 'unsafe-inline' https://js.stripe.com",
       "style-src 'self' 'unsafe-inline'",
       "img-src 'self' data: blob: https://*.stripe.com",
       "font-src 'self'",
